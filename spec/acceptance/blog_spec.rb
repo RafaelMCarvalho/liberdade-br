157a8f3263a68d8e6f09d720bc9e62c283bad489
Eduardo Hertz <eduardo@algorich.com.br>
2012-07-16 14:39:16 -0300

    Adicionados mais alguns testes para o modelo Blog.

--- spec/acceptance/blog_spec.rb
+++ spec/acceptance/blog_spec.rb
@@ -28,4 +28,47 @@ feature 'manager blogs' do
 			page.should have_content 'Link não pode ser vazio'
 		end
 	end
+
+	context 'edit a blog' do
+		background do
+			@blog = FactoryGirl.create :blog
+			click_link 'Blog'
+			click_link 'Editar'
+		end
+
+		it 'successfully' do
+			fill_in 'Nome', :with => 'Outro nome'
+			fill_in 'Link', :with => 'http://blog.meuevento.com'
+			fill_in 'RSS', :with => 'http://blog.meuevento.com/rss.xml'
+			fill_in 'Descrição', :with => 'MeuEvento.com é um sistema de gerenciamento de evento'
+			click_button 'Salvar'
+			page.should have_content 'Blog atualizado(a) com sucesso'
+			@blog.reload
+			@blog.name.should == 'Outro nome'
+			@blog.link.should == 'http://blog.meuevento.com'
+			@blog.rss.should == 'http://blog.meuevento.com/rss.xml'
+			@blog.description.should == 'MeuEvento.com é um sistema de gerenciamento de evento'
+		end
+
+		it 'with errors' do
+			fill_in 'Nome', :with => ''
+			fill_in 'Link', :with => ''
+			click_button 'Salvar'
+			page.should have_content 'Nome não pode ser vazio'
+			page.should have_content 'Link não pode ser vazio'
+		end
+	end
+
+	context 'delete a blog' do
+		background do
+			@blog = FactoryGirl.create :blog
+			click_link 'Blog'
+			click_link 'Excluir'
+		end
+
+		it 'successfully' do
+			click_button 'Sim, eu tenho certeza'
+			page.should have_content 'Blog excluído(a) com sucesso'
+		end
+	end
 end

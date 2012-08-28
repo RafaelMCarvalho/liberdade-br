e179895f1191a0f633f7148493ac54959e44596a
Herond Robaina Salles <herond@herond.(none)>
2012-08-28 17:22:25 -0300

    Ajustado banners.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,30 +29,33 @@ $(function () {
 var banner_image_sld, banner_info_sld, sponsors_sld, partners_sld;
 
 $(function () {
-    banner_image_sld = $("#banner ul.images").bxSlider({
-        auto: true, controls: false, pause: 6000,
-        onBeforeSlide: function (curr_sld_num, slt_qty, curr_html) {
-            $("#banner .controls > span").removeClass("active");
-            $($("#banner .controls > span")[curr_sld_num]).addClass("active");
-        }
-    });
+    if (!$("#banner_wrapper").hasClass('no_slide'))
+      banner_image_sld = $("#banner ul.images").bxSlider({
+          auto: true, controls: false, pause: 6000,
+          onBeforeSlide: function (curr_sld_num, slt_qty, curr_html) {
+              $("#banner .controls > span").removeClass("active");
+              $($("#banner .controls > span")[curr_sld_num]).addClass("active");
+          }
+      });
 });
 
 $(function () {
-    banner_info_sld = $("#banner .infos ul").bxSlider({
-        auto: true, controls: false, mode: "fade", pause: 6000
-    });
+    if (!$("#banner_wrapper").hasClass('no_slide'))
+      banner_info_sld = $("#banner .infos ul").bxSlider({
+          auto: true, controls: false, mode: "fade", pause: 6000
+      });
 });
 
 $(function () {
-    $("#banner .controls > span").click(function () {
-        var slide_pos = $(this).index();
+    if (!$("#banner_wrapper").hasClass('no_slide'))
+      $("#banner .controls > span").click(function () {
+          var slide_pos = $(this).index();
 
-        $("#banner .controls > span").removeClass("active");
-        $(this).addClass("active");
-        banner_image_sld.goToSlide(slide_pos);
-        banner_info_sld.goToSlide(slide_pos);
-    });
+          $("#banner .controls > span").removeClass("active");
+          $(this).addClass("active");
+          banner_image_sld.goToSlide(slide_pos);
+          banner_info_sld.goToSlide(slide_pos);
+      });
 });
 
 $(function () {

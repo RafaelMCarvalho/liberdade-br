62fd64d69ff57c245b1582ecb653d0763693a59c
Eduardo Hertz <eduardo@algorich.com.br>
2012-08-28 09:05:35 -0300

    Adicionado ckeditor para precompilar os assets.

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'simple_form', '2.0.2'
 gem 'slim', '1.2.2'
 gem 'jquery-rails', '2.0.2'
 gem 'sprockets', '2.1.3'
-gem 'ckeditor'
+gem 'ckeditor', '3.7.1'
 gem 'paperclip', '2.7.0'
 gem 'feedzirra', '0.2.0.rc2'
 gem 'whenever', '0.7.3', :require => false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,7 @@ LiberdadeBr::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( login.css )
+  config.assets.precompile += Ckeditor.assets
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
